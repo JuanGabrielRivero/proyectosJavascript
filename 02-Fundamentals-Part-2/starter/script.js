be2f6640afbbd9c76d1a424b3a59792b980263d0
juani <jrm3301@gmail.com>
2023-02-03 15:43:16 +0100

    while loop roll dice

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -425,3 +425,9 @@ while (rep <= 10){
     console.log(`Lifting weights repetition ${rep}`);
     rep++;
 }
+
+// let rep = 1;
+// while (rep <= 10){
+//     console.log(`Lifting weights repetition ${rep}`);
+//     rep++;
+// }
